036df6715fc718a5c8a74cff766480997c2e8ac3
kkatriinaS <katrina.sleze@gmail.com>
2024-04-24 12:05:23 +0300

    Pievienots Security config

--- src/main/java/lv/backend/BackendApplication.java
+++ src/main/java/lv/backend/BackendApplication.java
@@ -5,6 +5,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lv.backend.models.ParkingArea;
+import lv.backend.models.ParkingSpot;
+import lv.backend.models.Reservation;
+import lv.backend.models.SpotStatus;
 import lv.backend.models.users.User;
 import lv.backend.repos.IParkingAreaRepo;
 import lv.backend.repos.IParkingSpotRepo;
@@ -25,7 +29,30 @@ public class BackendApplication {
 			@Override
 			public void run(String... args) throws Exception {
 				User us1 = new User(0, "Janis", "janis123", "000", "janisp@gmail.com", null);
+				User us2 = new User(0, "Ieva", "iozola", "1188", "ievao@gmail.com", null);
 				userRepo.save(us1);
+				userRepo.save(us2);
+				
+				ParkingArea pa1 = new ParkingArea(0, "Arēna Rīga", 1000, null);
+				ParkingArea pa2 = new ParkingArea(0, "Latgales Vēstniecība GORS", 150, null);
+				parkingAreaRepo.save(pa1);
+				parkingAreaRepo.save(pa2);
+				
+				ParkingSpot ps1 = new ParkingSpot(0, SpotStatus.FREE, null, pa1);
+				ParkingSpot ps2 = new ParkingSpot(0, SpotStatus.FREE, null, pa1);
+				ParkingSpot ps3 = new ParkingSpot(0, SpotStatus.TAKEN, null, pa1);
+				ParkingSpot ps4 = new ParkingSpot(0, SpotStatus.FREE, null, pa1);
+				ParkingSpot ps5 = new ParkingSpot(0, SpotStatus.TAKEN, null, pa2);
+				ParkingSpot ps6 = new ParkingSpot(0, SpotStatus.FREE, null, pa2);
+				parkingSpotRepo.save(ps1);
+				parkingSpotRepo.save(ps2);
+				parkingSpotRepo.save(ps3);
+				parkingSpotRepo.save(ps4);
+				parkingSpotRepo.save(ps5);
+				parkingSpotRepo.save(ps6);
+				
+				Reservation res1 = new Reservation(0, null, null, us2, ps5);
+				reservationRepo.save(res1);
 
 			}
 
